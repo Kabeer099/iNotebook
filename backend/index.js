493f454c92caa07a59dc508d8e3b597bb14ffa3c
kabeer099 <devilhenry670@gmail.com>
2023-07-11 18:26:16 +0500

    routes and schema built

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,10 @@ connectToMongo();
 
 const app = express()
 const port = 3000
+// Available routes
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
